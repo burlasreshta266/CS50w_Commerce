e829f5a988d75a04782ff522c9d97d9ec2cba519
Sreshta <burlasreshta26@gmail.com>
2025-05-28 23:27:09 +0530

    created listing page, category page and bidding system

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create_listing", views.create_listing, name="create_listing"),
+    path("listing_page/<int:id>", views.listing_page, name='listing_page'),
+    path("place_bid/<int:id>", views.place_bid, name='place_bid'),
+    path("categories", views.all_categories, name='all_categories'),
+    path("categoty_list/<int:id>", views.category_list, name='category_list'),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -3,19 +3,25 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 from django import forms
 from .models import User, Listing, Category
 
 
 def index(request):
-    user = request.user
-    if len(user.listings.all())==0:
-        return render(request, "auctions/index.html", {
-            "listings" : ["no listings"]
-        })
+    if request.user.is_authenticated:
+        user = request.user
+        if len(user.listings.all())==0:
+            return render(request, "auctions/index.html", {
+                "listings" : ["no listings"]
+            })
+        else:
+            return render(request, "auctions/index.html", {
+                "listings" : user.listings.all()
+            })
     else:
         return render(request, "auctions/index.html", {
-            "listings" : user.listings.all()
+                "listings" : Listing.objects.all()
         })
 
 def login_view(request):
@@ -69,11 +75,8 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
-categories = []
-for category in Category.objects.all():
-    categories.append(category.name)
 
-# Create a new Listing
+# Listing form
 class NewListingForm(forms.Form):
     title = forms.CharField(widget=forms.TextInput(attrs={
         'style': 'width:70%', 'class': 'form-control', 'placeholder':'Enter the name of the product',
@@ -93,6 +96,14 @@ class NewListingForm(forms.Form):
         'style': 'width:70%', 'class': 'form-control', 'placeholder':'Enter url of the product image'
     }))
 
+# Bid form 
+class BidForm(forms.Form):
+    bid = forms.DecimalField(min_value=1.00, widget=forms.NumberInput(attrs={
+        'style': 'width:70%', 'class': 'form-control', 'placeholder':'Make sure bid is greater than price'
+    }))
+
+
+# Create a new Listing
 def create_listing(request):
     if request.method == 'POST':
         form = NewListingForm(request.POST)
@@ -114,4 +125,66 @@ def create_listing(request):
     else:
         return render(request, "auctions/create_listing.html",{
             "form" : NewListingForm()
+        })
+    
+
+# Listing page
+def listing_page(request, id):
+    listing = Listing.objects.all().get(pk=id)
+    return render(request, "auctions/listing_page.html", {
+        "listing": listing,
+        "bid_form" : BidForm()
+    })
+
+
+# Place bid
+def place_bid(request, id):
+    listing = Listing.objects.all().get(pk=id)
+    if not request.user.is_authenticated:
+        return render(request, "auctions/listing_page.html", {
+                "listing": listing,
+                "bid_form" : BidForm(),
+                "message" : "not sign in"
+        })
+    else:
+        if request.method=="POST":
+            form = BidForm(request.POST)
+            if form.is_valid():
+                placed_bid = form.cleaned_data["bid"]
+                if placed_bid>listing.price:
+                    listing.price = placed_bid
+                    listing.save()
+                    return render(request, "auctions/listing_page.html", {
+                        "listing": listing,
+                        "bid_form" : form,
+                        "message" : "success"
+                    })
+                else:
+                    return render(request, "auctions/listing_page.html", {
+                        "listing": listing,
+                        "bid_form" : form,
+                        "message" : "fail"
+                    })
+            else:
+                return render(request, "auctions/listing_page.html", {
+                "listing": listing,
+                "bid_form" : form,
+                "message" : "fail"
+                })
+        else:
+            return HttpResponseRedirect(reverse(listing_page))
+    
+# List of Categories
+def all_categories(request):
+    return render(request, "auctions/all_categories.html", {
+        "categories" : Category.objects.all()
+    })
+
+def category_list(request, id):
+    category = Category.objects.all().get(pk=id)
+    if len(category.listings.all())==0:
+        return render(request, "auctions/error.html")
+    else:
+        return render(request, "auctions/index.html", {
+            "listings" : category.listings.all()
         })
